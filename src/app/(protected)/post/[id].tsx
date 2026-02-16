25937299f34c7fea17784cb02be3d72856b00398
Anshul Batra <anshubatra2563@gmail.com>
2026-02-16 18:47:18 +0530

    add basic implementation of comments

--- src/app/(protected)/post/[id].tsx
+++ src/app/(protected)/post/[id].tsx
@@ -1,10 +1,16 @@
-import { View, Text } from "react-native"
+import { View, Text, FlatList } from "react-native"
 import { useLocalSearchParams } from "expo-router"
 import posts from "../../../../assets/data/posts.json"
 import PostListItem from "../../../components/PostListItem"
+import comments from "../../../../assets/data/comments.json"
+import CommentListItem from "../../../components/CommentListItem"
+
 export default function DetailedPost() {
     const { id } = useLocalSearchParams()
     const detailedPost = posts.find((post) => post.id === id)
+
+    const postComments = comments.filter((comment) => comment.post_id === "post-1")
+    console.log(postComments)
 
     if(!detailedPost) {
         return <Text>Post Not Found</Text>
@@ -12,7 +18,16 @@ export default function DetailedPost() {
 
     return (
         <View>
-            <PostListItem post={detailedPost} isDetailedPost />
+            
+            <FlatList 
+                data={postComments}
+                renderItem={({ item }) => (
+                    <CommentListItem comment={item}/>
+                )}
+                ListHeaderComponent={
+                    <PostListItem post={detailedPost} isDetailedPost />
+                }
+            />
         </View>
     )
 }
